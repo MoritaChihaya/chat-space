789704161717dbf4d796e8c44197753c9107d17e
ChihayaMorita <chihayamorita1027@gmail.com>
2020-06-22 03:21:39 +0900

    非同期化通信の実装
    非同期化通信の実装を行う

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image){
+      var html =
+        `<div class="message">
+          <div class="upper-info">
+            <div class="upper-info--user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-info--posting-date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    }else{
+      var html =
+      `<div class="message">
+        <div class="upper-info">
+          <div class="upper-info--user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-info--posting-date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('.send-btn').prop('disabled', false);
+    })
+    .fail(function() {
+      alert('メッセージの送信に失敗しました');
+      $('.send-btn').prop('disabled', false);
+    });
+  });
+});
